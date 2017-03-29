1d4ae1137f85e9d2a10ceb9ccd6b6abc17d40709
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-03-29 18:04:41 +0200

    Started Random Quote Machine

--- AjaxTut/script.js
+++ AjaxTut/script.js
@@ -6,29 +6,43 @@ var animal = document.getElementById("animal-info");
 
 
 btn.addEventListener('click', function(){
-		// Ajax request
+	// open new Ajax request
 	var req = new XMLHttpRequest();
+
+	// Get data from server and get JSON file
 	req.open("GET", "https://learnwebcode.github.io/json-example/animals-" + page + ".json");
+	// Load data 
 	req.onload = function(){  // onload is the modern version of onreadystatechange
-		var resp = JSON.parse(req.responseText);
+		// the response should be parsed and outputted 
+		if (req.status >= 200 && req.status < 400){
+					var resp = JSON.parse(req.responseText);
+		// and finally rendered with HTML around it!
 		renderHTML(resp);
+		}
+		else{
+			console.log("Error! Error! Error!");
+		}
+
 	};
+	// Error handling
+	req.onerror = function(){
+		console.log("Connection error");
+	};
+	// Send request
 	req.send();
 	page++;
+	// Add class to button in case the page number is more than 3
 	if(page > 3){
 		btn.classList.add("hide-me");
 	}
 });
 
 
-function makeAjaxRequest(method, url){
-
-}
-
+// Function to render HTML - nice and clean
 function renderHTML(data){
 	var HTMLString = "";
 	for (var i = 0; i < data.length; i++) {
-		HTMLString += "<p>" + data[i]["name"] + "is a " + data[i].species + "</p>"
+		HTMLString += "<p>" + data[i]["name"] + " is a " + data[i].species + "</p>"
 	}
 
 	animal.insertAdjacentHTML("beforeend", HTMLString);
---
+++ RandomQuote/script.js
@@ -0,0 +1,39 @@
+
+function makeRequest(method, url){
+    var request = new XMLHttpRequest();
+    request.open(method, url);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var response = JSON.parse(request.responseText);
+            // response.classList.add(".hide");
+            renderHTML(response);
+
+
+        } else
+            console.log("Error in dealing with request");
+    };
+    request.onerror = function() {
+        console.log("Connection error");
+    };
+    request.send();
+}
+
+function renderHTML(data){
+    var joke = data.value['joke']; 
+    var HTMLstring = "<p>" + joke + "</p>";
+    quote.insertAdjacentHTML("beforeend", HTMLstring);
+}
+
+
+
+// Get button
+var btn = document.querySelector(".btn");
+var quote = document.querySelector(".quote")
+
+
+btn.addEventListener('click', function() {
+	var url ="http://api.icndb.com/jokes/random";
+    makeRequest('GET', url);
+});
+
+
