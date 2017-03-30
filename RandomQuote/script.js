d15056f235a967489c9774c1c9ffb9efb2175dbc
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-03-30 17:50:17 +0200

    Finished Random Quote Machine project

--- RandomQuote/script.js
+++ RandomQuote/script.js
@@ -1,14 +1,38 @@
+// Variables
+var count = 0;
+var btn = document.querySelector(".btn");
+var quote = document.querySelector(".quote")
 
+
+btn.addEventListener('click', function() {
+makeRequest('GET', "http://api.icndb.com/jokes/random");
+});
+
+if(count < 1){
+    makeRequest('GET', "http://api.icndb.com/jokes/random");
+    count++;
+}
+
+function makeTweetUrl(quoteString){
+    var spl = quoteString.split(" ");
+    spl = spl.join("%20");
+    var tweet = document.querySelector(".twitter-share-button");
+    var tweetURL = tweet.href + "?text=" + spl + "&hashtags=FreeCodeCamp";
+    return tweet.href = tweetURL;
+}
+
+
+
+// FUNCTION DECLARATIONS
+// Ajax request function
 function makeRequest(method, url){
     var request = new XMLHttpRequest();
     request.open(method, url);
     request.onload = function() {
         if (request.status >= 200 && request.status < 400) {
             var response = JSON.parse(request.responseText);
-            // response.classList.add(".hide");
             renderHTML(response);
-
-
+            makeTweetUrl(quote.textContent)
         } else
             console.log("Error in dealing with request");
     };
@@ -16,24 +40,11 @@ function makeRequest(method, url){
         console.log("Connection error");
     };
     request.send();
-}
 
+}
+// Renders HTML
 function renderHTML(data){
     var joke = data.value['joke']; 
-    var HTMLstring = "<p>" + joke + "</p>";
+    var HTMLstring = "<p class='text'>" + joke + "</p>";
     quote.innerHTML = HTMLstring;
 }
-
-
-
-// Get button
-var btn = document.querySelector(".btn");
-var quote = document.querySelector(".quote")
-
-
-btn.addEventListener('click', function() {
-	var url ="http://api.icndb.com/jokes/random";
-    makeRequest('GET', url);
-});
-
-
