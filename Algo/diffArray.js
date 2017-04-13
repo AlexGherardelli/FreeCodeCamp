bf53aeea01c3d7360dde06e0fac0302dada80226
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-04-13 17:34:42 +0200

    Ajax response + partial DOM manipulation + partial responsiveness

---
+++ Algo/diffArray.js
@@ -0,0 +1,6 @@
+function diffArray(arr1, arr2) {
+  var newArr = [];
+  // Same, same; but different.
+  return newArr;
+}
+
--- WikiViewer/scripts.js
+++ WikiViewer/scripts.js
@@ -1,9 +1,51 @@
-// https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch='
+function makeRequest(method, url, callback){
+	var request = new XMLHttpRequest();
+	request.open(method, url);
+	request.onload = function(){
+		if(request.status >= 200 && request.status < 400){
+			var response = JSON.parse(request.responseText);
+			callback(response);
+		}
+		else{
+			response = JSON.parse(request.responseText);
+			console.warn("Error in dealing with request");
+		}
+	};
+	request.onerror = function(){
+		console.warn("Connection error");
+	};
+	request.send();
+}
+
+function renderHTML(data){
+	response = data;
+	var wiki = document.querySelector(".wikipedia");
+	console.log(wiki);
+	for (var i = 0; i < data.query.search.length; i++) {
+		console.log("Writing data n: " + [i]);
+		var entry = "<div class='entry'><h2>" + data.query.search[i].title + "</h2><p class='snippet'>" + data.query.search[i].snippet + "</p></div>";
+		wiki.insertAdjacentHTML("beforeend", entry);
+	}
+
+}
+
+
+
+
 
 var searchbox = document.querySelector(".search-input");
 var submit = document.querySelector("#sub");
 
+var response = "";
+
 submit.addEventListener("click", function(){
-  // console.log("Pressing the button");
-  console.log(searchbox.value);
+	var search = searchbox.value.split(" ").join("+");
+	var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&utf8=1&srsearch=" + search;
+
+	makeRequest("GET", url, renderHTML);
 });
+
+
+
+
+	
