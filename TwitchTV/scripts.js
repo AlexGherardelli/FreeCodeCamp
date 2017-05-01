13ebb068b5bfa05199600af50beb19671732cec1
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-05-01 14:32:43 +0200

    Completed basic functionalities for twitch TV

--- TwitchTV/scripts.js
+++ TwitchTV/scripts.js
@@ -1,56 +1,64 @@
-var streamers = ["blizzheroes", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
-
-var twitch = document.querySelector(".twitch");
-
 // DO STUFF HERE
-for(var i = 0; i < streamers.length; i++){
-	var url = "https://wind-bow.glitch.me/twitch-api/streams/" + streamers[i];
-	makeRequest("GET", url, renderHTML);
-}
+var streamers = ["blizzheroes", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin"];
+var twitch = document.querySelector(".twitch");
+var stream_url = "https://wind-bow.glitch.me/twitch-api/streams/";
+var user_url = "https://wind-bow.glitch.me/twitch-api/users/";
+var html = "";
+var offline_streamers = [];
 
+for (var i = 0; i < streamers.length; i++) {
+    var url = stream_url + streamers[i];
+    getData("GET", url, updateUsers);
+}
 
 // FUNCTION DEFINITIONS
-function renderHTML(data){
-	// console.log(data);
-	var htmlString = "";
-	if(data.stream != null){
-		htmlString = "<div class='element online'><p>" + data.stream.channel.status + "</p>" +
-		"<p>" + data.stream.game + "</p></div>";
-		twitch.insertAdjacentHTML("beforeEnd", htmlString);
-	}
-	else{
-		// console.log("Channel is offline or has been deleted");
-		for(var i = 0; i < streamers.length; i++){
-			console.log(streamers.length);
-			var url = "https://wind-bow.glitch.me/twitch-api/users/" + streamers[i];
-			// console.log(url);
-			makeRequest("GET", url, function(dat){
-				if(dat.display_name != undefined ){
-					// Buggy call (calling multiple times)!
-					twitch.insertAdjacentHTML('beforeEnd', "<div class='element inexistent'><p>" + dat.message + "</p> </div>");
-				}
-			});
-		}
-}
+
+// Make ajax request to get data and process it
+function getData(method, url, callback) {
+    var request = new XMLHttpRequest();
+    request.open(method, url);
+    request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+            var response = JSON.parse(request.responseText);
+            window.response = response;
+            callback(response);
+        } else {
+            response = JSON.parse(request.responseText);
+            console.warn("Error in dealing with request");
+        }
+    };
+    request.onerror = function() {
+        console.warn("Connection error");
+    };
+    request.send();
 }
 
+// Checks if user is online and renders data
+function updateUsers(data) {
+    // if users are not streaming
+    if (data.stream == null) {
+        // updates offline users
+        updateOfflineUsers(data._links.channel.substr(38));
+    } else {
+        html = "<div class='element online'><p>" + data.stream.channel.status + "</p>" +
+            "<p>" + data.stream.game + "</p></div>";
+        twitch.insertAdjacentHTML("beforeEnd", html);
+    }
+}
 
-function makeRequest(method, url, callback){
-	var request = new XMLHttpRequest();
-	request.open(method, url);
-	request.onload = function(){
-		if(request.status >= 200 && request.status < 400){
-			var response = JSON.parse(request.responseText);
-      window.response = response;
-			callback(response);
+// Get data from offline users
+function updateOfflineUsers(username) {
+	// console	.log("Offline user: " + username);
+	var url = user_url + username;
+	// console.log(url);
+	getData("GET", url, function(data){
+		// console.log(data);
+		if(data.status === 404){
+			twitch.insertAdjacentHTML('beforeEnd', "<div class='element inexistent'><p> User"+ username + " was not found </p> </div>");
 		}
 		else{
-			response = JSON.parse(request.responseText);
-			console.warn("Error in dealing with request");
+      html = "<div class='element offline'><p>" + username +  " is currently offline</p></div>";
+      twitch.insertAdjacentHTML("beforeEnd", html);
 		}
-	};
-	request.onerror = function(){
-		console.warn("Connection error");
-	};
-	request.send();
+	});
 }
