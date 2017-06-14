5921e995cf9e3670aaf8cfd387092b2abc1168f2
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-06-14 12:18:33 +0200

    Finished inventory update

--- Algo/inventory-update.js
+++ Algo/inventory-update.js
@@ -1,11 +1,12 @@
 /*
-Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
+Compare and update the oldItems stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing oldItems item quantities (in arr1). If an item cannot be found, add the new item and quantity into the oldItems array. The returned oldItems array should be in alphabetical order by item.
 */
 
 
 function updateInventory(arr1, arr2) {
-    // All inventory must be accounted for or you're fired!
+    // All oldItems must be accounted for or you're fired!
 
+    // Convert arr1 into object
     var oldItems = arr1.reduce(function(done, next){
         var key = next[1];
         var value = next[0];
@@ -13,6 +14,7 @@ function updateInventory(arr1, arr2) {
         return done;
     }, {});
 
+    // Convert arr2 into object
     var newItems = arr2.reduce(function(done, next){
         var key = next[1];
         var value = next[0];
@@ -20,30 +22,34 @@ function updateInventory(arr1, arr2) {
         return done;
     },{});
 
-    console.log(oldItems);
-    console.log(newItems);
 
+    // Arrays of keys of all new items
+    var keys = Object.keys(newItems);
 
-    // var obj = Object.assign(oldItems, newItems;
+    keys.forEach(function(e){
+        // if there is an item in the oldItems
+        if(oldItems.hasOwnProperty(e)){
+            // updates the oldItems by adding the new quantity to the old one
+            var val = oldItems[e];
+            oldItems[e] = val + newItems[e];
+        }
+        else{
+            oldItems[e] = newItems[e];
+        }
+    });
 
+    // Sort oldItems alphabetically
+    var inventory = {}
+    Object.keys(oldItems).sort().forEach(function(key) {
+      inventory[key] = oldItems[key];
+    });
 
+    var newInventory = [];
 
-
+    for(var prop in inventory){
+        newInventory.push([inventory[prop], prop.toString()]);
+    }
+    return  newInventory;
 }
 
-// Example inventory lists
-var curInv = [
-    [21, "Bowling Ball"],
-    [2, "Dirty Sock"],
-    [1, "Hair Pin"],
-    [5, "Microphone"]
-];
-
-var newInv = [
-    [2, "Hair Pin"],
-    [3, "Half-Eaten Apple"],
-    [67, "Bowling Ball"],
-    [7, "Toothpaste"]
-];
-
-updateInventory(curInv, newInv);
+// Example oldItems lists
