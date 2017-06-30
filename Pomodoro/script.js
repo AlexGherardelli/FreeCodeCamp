e506d810d71862bede1c15e1824db9d8bc64a77b
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-06-30 17:11:45 +0200

    Added tomato icon

--- Pomodoro/script.js
+++ Pomodoro/script.js
@@ -11,6 +11,8 @@ $(document).ready(function() {
     var break_timer = 5;
     var seconds = 0;
 
+
+    // Select between Break and Session
     $break.click(function() {
         $(".timer").addClass("hidden");
         $(".break").removeClass("hidden");
@@ -26,21 +28,11 @@ $(document).ready(function() {
         $break.removeClass("whiten");
     });
 
-    $play.click(function() {
-        $pause.removeClass("hidden");
-        $play.addClass("hidden");
-    });
-
-    $pause.click(function() {
-        $pause.addClass("hidden");
-        $play.removeClass("hidden")
-    });
-
-
+    // Add minutes to session or break timer
     $('.session-minus').click(function() {
         session_timer--;
-        if(session_timer < 1){
-        	session_timer = 1;
+        if (session_timer < 1) {
+            session_timer = 1;
         }
         $('.session-time').html(session_timer + ":00");
     });
@@ -54,9 +46,9 @@ $(document).ready(function() {
 
     $('.break-minus').click(function() {
         break_timer--;
-			if(break_timer < 1){
-			break_timer = 1;
-			}
+        if (break_timer < 1) {
+            break_timer = 1;
+        }
         $('.break-time').html(break_timer + ":00");
     });
     $('.break-plus').click(function() {
@@ -64,5 +56,39 @@ $(document).ready(function() {
         $('.break-time').html(break_timer + ":00");
     });
 
+
+    // Set functionality for play, pause and stop
+    $play.click(function() {
+        $pause.removeClass("hidden");
+        $play.addClass("hidden");
+        $('.btn').addClass("hidden");
+        startTimer();
+    });
+
+    $pause.click(function() {
+        $pause.addClass("hidden");
+        $play.removeClass("hidden");
+        $('.btn').removeClass("hidden");
+        pauseTimer();
+    });
+    $stop.click(function() {
+        $('.btn').removeClass("hidden");
+        stopTimer();
+    });
+
+
+    function startTimer() {
+        console.log("Timer starter, counting down!");
+        session_timer = $('.session-time').text();
+        console.log(session_timer);
+
+
+    }
+    function stopTimer(){
+    	console.log("Timer stopped!");
+    }
+    function pauseTimer(){
+    	console.log("Timer in pause");
+    }
 
 });
