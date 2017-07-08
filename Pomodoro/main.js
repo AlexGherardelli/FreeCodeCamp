aa1a3871463d1fb5caa35539b526db1dfc2bb60d
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-07-08 21:23:16 +0200

    Added stop button

--- Pomodoro/main.js
+++ Pomodoro/main.js
@@ -1,47 +1,13 @@
-function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
-    console.log(timer);
-    console.log(duration);
-    console.log(minutes);
-    console.log(seconds);
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-display.textContent = minutes + ":" + seconds;
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-}
-
-$(document).ready(function(){
-  var display = document.querySelector(".session-time");
-  var min = 25;
-  var duration = min * 60;
+var pomodoro = {
+  init: function(){
+      console.log("Initialized");
+  },
+  hello: console.log(this);
+};
 
 
-  $(".fa-play").click(function(){
-    console.log("Clicked");
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+window.onload = function(){
+  pomodoro.init();
+  pomodoro.hello;
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-display.textContent = minutes + ":" + seconds;
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
-
-  })
-
-
-
-
-
-});
+};
--- Pomodoro/script.js
+++ Pomodoro/script.js
@@ -5,13 +5,16 @@ $(document).ready(function() {
     var session_btn = $('#session-btn');
     var play = $(".fa-play");
     var pause = $(".fa-pause");
-    var stop = $(".fa-stop");
+    var $stop = $(".fa-stop");
     var break_timer = $('.break-time');
-    var display = $('.session-time')
+    var display = $('.session-time');
 
     var session_minutes = 25;
     var seconds = 0;
     var break_minutes = 5;
+
+    var timer;
+    var time;
 
     // Select between Break and Session
     break_btn.click(function() {
@@ -49,7 +52,7 @@ $(document).ready(function() {
         if (break_minutes < 1) {
             break_minutes = 1;
         }
-        
+
         $('.break-time').html(break_minutes + ":0" + seconds);
     });
     $('.break-plus').click(function() {
@@ -63,22 +66,10 @@ $(document).ready(function() {
         // pause.removeClass("hidden");
         // play.addClass("hidden");
         $('.btn').addClass("hidden");
-            console.log("Clicked");
-    var duration = session_minutes * 60;
-    var timer = duration,
-        minutes, seconds;
-    setInterval(function() {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.text(minutes + ":" + seconds);
-        if (--timer < 0) {
-            timer = duration;
-        }
-    }, 1000);
+        var duration = session_minutes * 60;
+        timer = duration,
+            session_minutes, seconds;
+        startTimer(duration, timer);
 });
 
     // pause.click(function() {
@@ -87,20 +78,30 @@ $(document).ready(function() {
     //     $('.btn').removeClass("hidden");
     //     pauseTimer();
     // });
-    stop.click(function() {
+  $stop.click(function() {
         $('.btn').removeClass("hidden");
-        stopTimer();
+        clearInterval(time);
     });
 
 
-    function startTimer(duration, display) {
-      console.log(duration);
-      console.log(display);
+    function startTimer(duration, timer) {
+      time = setInterval(function() {
+          session_minutes = parseInt(timer / 60, 10);
+          seconds = parseInt(timer % 60, 10);
+
+          seconds = seconds < 10 ? "0" + seconds : seconds;
+
+          display.text(session_minutes + ":" + seconds);
+          if (--timer < 0) {
+              timer = duration;
+          }
+      }, 1000);
     }
 
 
     function stopTimer(){
-    	console.log("Timer stopped!");
+    	display.text("KABOOM");
+
     }
     function pauseTimer(){
     	console.log("Timer in pause");
