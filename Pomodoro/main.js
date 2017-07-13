f1554dd8b791430ee9a90dc400b7b4500a6b5ce7
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-07-13 17:02:09 +0200

    Refactoring in main

--- Pomodoro/main.js
+++ Pomodoro/main.js
@@ -1,13 +1,36 @@
-var pomodoro = {
-  init: function(){
-      console.log("Initialized");
-  },
-  hello: console.log(this);
-};
+// var Pomodoro = {
+// 	session_time: 25,
+// 	break_time: 5, 
+// 	inSession: false,
+// 	inBreak: false,
+// 	startTimer: function(time){
+// 		console.log(time);
+// 		console.log("Timer started");
+// 	},
+// 	pauseTimer: function(time){
+// 		console.log("Timer paused");
+// 	},
+// 	resetTimer: function(time){
+// 		console.log("Timer reset");
+// 	}
 
+// };
 
-window.onload = function(){
-  pomodoro.init();
-  pomodoro.hello;
+function Pomodoro(){
+	this.session_minutes = 25;
+	this.seconds = 0;
+	this.inSession = false;
+	this.duration = (this.minutes * 60) + this.seconds;
+	this.startTimer = function(){
+		console.log(this.duration);
+	};
+	this.pauseTimer = function(){
+		console.log("Timer paused");
+	};
+	this.resetTimer = function(){
+		console.log("Timer reset");
+	};
+}
 
-};
+var sessionClock = new Pomodoro();
+var breakClock = new Pomodoro();
