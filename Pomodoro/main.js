1ea9c04a79905473683d5cd95c39ab4895e397f4
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-07-14 15:43:49 +0200

    OOP Pomodoro setup done. Stop and play function work

--- Pomodoro/main.js
+++ Pomodoro/main.js
@@ -1,81 +1,124 @@
+var isBreakTime = false;
 
-    function Pomodoro() {
-        this.minutes = 1;
+function Pomodoro(time) {
+    this.minutes = time;
+    this.seconds = 0;
+    this.countdown;
+    var that = this; // private this
+    this.startTimer = function() {
+    		that.duration = (this.minutes * 60) + this.seconds;
+    		that.timer = this.duration;
+        that.countdown = setInterval(function() {
+            var min = parseInt(that.timer / 60, 10);
+            var sec = parseInt(that.timer % 60, 10);
+
+            sec = sec < 10 ? "0" + sec : sec;
+            display.text(min + ":" + sec);
+
+            // display.text(minutes + ":" + sec);
+            if (--that.timer < 0) {
+                that.timer = that.duration;
+                isBreakTime = true;
+                clearInterval(that.countdown);
+            }
+
+        }, 1000);
+    };
+    this.pauseTimer = function() {
+        clearInterval(that.countdown);
+    };
+    this.resetTimer = function() {
+        clearInterval(that.countdown);
         this.seconds = 0;
-        this.startBreak = false;
-        this.duration = (this.minutes * 60) + this.seconds;
-        this.timer = this.duration;
-        this.countdown;
-        var that = this; // private this
-        this.startTimer = function() {
-            that.countdown = setInterval(function() {
-                var min = parseInt(that.timer / 60, 10);
-                var sec = parseInt(that.timer % 60, 10);
+        display.text(this.minutes + ":00");
+    };
+}
 
-                sec = sec < 10 ? "0" + sec : sec;
-                console.log(min + ":" + sec);
+// intialize two new Pomodoros
+var sessionClock = new Pomodoro(1);
+var breakClock = new Pomodoro(5);
 
-                // display.text(minutes + ":" + sec);
-                if (--that.timer < 0) {
-                    that.timer = that.duration;
-                    that.startBreak = true;
-                    clearInterval(that.timer);
-                }
 
-            }, 1000);
-        };
-        this.pauseTimer = function() {
-            clearInterval(that.countdown);
-        };
-        this.resetTimer = function() {
-            clearInterval(that.countdown);
-        };
-    }
+// Display session minutes
+var display = $('.session-time');
+var break_timer = $('.break-time');
+display.html(sessionClock.minutes + ":00");
+$('.break-time').html(breakClock.minutes + ":00");
 
-    // intialize two new Pomodoros
-    var sessionClock = new Pomodoro();
-    var breakClock = new Pomodoro();
-    // breakClock set to five minutes
-    breakClock.minutes = 5;
+// Control buttons
+var play = $(".fa-play");
+var pause = $(".fa-pause");
+var reset = $(".fa-stop");
 
-    // Display session minutes
-    var display = $('.session-time');
-    var break_timer = $('.break-time');
+// Switch between break and session
+$("#break-btn").click(function() {
+    $(".timer").addClass("hidden");
+    $(".break").removeClass("hidden");
+    $("#break-btn").addClass("whiten");
+    $('#session-btn').removeClass("whiten");
+
+});
+
+$('#session-btn').click(function() {
+    $(".break").addClass("hidden");
+    $(".timer").removeClass("hidden");
+    $('#session-btn').addClass("whiten");
+    $("#break-btn").removeClass("whiten");
+});
+
+// Add minutes to pomodoro session
+$('.session-minus').click(function() {
+    sessionClock.minutes--;
+    if (sessionClock.minutes < 1)
+        sessionClock.minutes = 1;
     display.html(sessionClock.minutes + ":00");
-    // display.html(breakClock.minutes + ":00");
+});
+// Remove minutes to pomodoro session
+$('.session-plus').click(function() {
+    sessionClock.minutes++;
+    display.html(sessionClock.minutes + ":00");
+});
+// Add minutes to break session
+$('.break-minus').click(function(){
+	breakClock.minutes--;
+	if(breakClock.minutes < 1){
+		breakClock.minutes = 1;
+	}
+	$('.break-time').text(breakClock.minutes + ":00");
+
+});
+// Remove minutes to break session
+$('.break-plus').click(function(){
+	breakClock.minutes++;
+	$(".break-time").text(breakClock.minutes + ":00");
+
+});
+
+// if play is clicked, start sessionClock
+play.click(function(){
+	pause.removeClass("hidden");
+	play.addClass("hidden");
+	$('.btn').addClass("hidden");
+	sessionClock.startTimer();
+});
 
 
-    $('.session-minus').click(function() {
-        sessionClock.minutes--;
-        if (sessionClock.minutes < 1) {
-            sessionClock.minutes = 1;
-        }
-        display.html(sessionClock.minutes + ":00");
-    });
-    $('.session-plus').click(function() {
-        sessionClock.minutes++;
-        display.html(sessionClock.minutes + ":00");
+// if pause is clicked, stop sessionClock
+pause.click(function(){
+	sessionClock.pauseTimer();
+	play.removeClass("hidden");
+	pause.addClass("hidden");
 
-    });
-
-
-    // Switch between break and session
-    $("#break-btn").click(function() {
-        $(".timer").addClass("hidden");
-        $(".break").removeClass("hidden");
-        $("#break-btn").addClass("whiten");
-        $('#session-btn').removeClass("whiten");
-        $('.break-time').html(breakClock.minutes + ":00");
-    });
-
-        $('#session-btn').click(function() {
-        $(".break").addClass("hidden");
-        $(".timer").removeClass("hidden");
-        $('#session-btn').addClass("whiten");
-        $("#break-btn").removeClass("whiten");
-    });
+});
+// if reset button is clicked, reset sessionClock and breakClock
+reset.click(function(){
+	sessionClock.resetTimer();
+	// breakClock.resetTimer();
+	play.removeClass("hidden");
+	pause.addClass("hidden");
+	$('.btn').removeClass("hidden");
+});
 
 
-
-
-
+// if isBreakTime is true 
+	// start BreakTimer
