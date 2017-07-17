b2fb9869341d770d035fb7aa4748cfcbb39a46a3
AlexGherardelli <alessandra.gherardelli@gmail.com>
2017-07-17 16:02:35 +0200

    Sort-of-working pomodoro clock

--- Pomodoro/main.js
+++ Pomodoro/main.js
@@ -1,13 +1,15 @@
+var startBreak = false;
+var b_time;
 
 function Pomodoro(time, display) {
     this.minutes = time;
-    this.seconds = 0;
+    this.seconds = 5;
     this.countdown;
     this.display = display;
     var that = this; // private this
     this.startTimer = function() {
-    		that.duration = (this.minutes * 60) + this.seconds;
-    		that.timer = this.duration;
+        that.duration = (this.minutes * 60) + this.seconds;
+        that.timer = this.duration;
         that.countdown = setInterval(function() {
             var min = parseInt(that.timer / 60, 10);
             var sec = parseInt(that.timer % 60, 10);
@@ -18,6 +20,7 @@ function Pomodoro(time, display) {
             if (--that.timer < 0) {
                 that.timer = that.duration;
                 clearInterval(that.countdown);
+
             }
 
         }, 1000);
@@ -37,9 +40,10 @@ var display = $('.session-time');
 var break_timer = $('.break-time');
 
 // intialize two new Pomodoros
-var sessionClock = new Pomodoro(1, display);
-var breakClock = new Pomodoro(5, break_timer);
+var sessionClock = new Pomodoro(0, display);
+var breakClock = new Pomodoro(0, break_timer);
 // breakClock set to five minutes
+
 
 
 display.html(sessionClock.minutes + ":00");
@@ -52,18 +56,11 @@ var reset = $(".fa-stop");
 
 // Switch between break and session
 $("#break-btn").click(function() {
-    $(".timer").addClass("hidden");
-    $(".break").removeClass("hidden");
-    $("#break-btn").addClass("whiten");
-    $('#session-btn').removeClass("whiten");
-
+    seeBreak();
 });
 
 $('#session-btn').click(function() {
-    $(".break").addClass("hidden");
-    $(".timer").removeClass("hidden");
-    $('#session-btn').addClass("whiten");
-    $("#break-btn").removeClass("whiten");
+    seeSession();
 });
 
 // Add minutes to pomodoro session
@@ -79,46 +76,68 @@ $('.session-plus').click(function() {
     display.html(sessionClock.minutes + ":00");
 });
 // Add minutes to break session
-$('.break-minus').click(function(){
-	breakClock.minutes--;
-	if(breakClock.minutes < 1){
-		breakClock.minutes = 1;
-	}
-	$('.break-time').text(breakClock.minutes + ":00");
+$('.break-minus').click(function() {
+    breakClock.minutes--;
+    if (breakClock.minutes < 1) {
+        breakClock.minutes = 1;
+    }
+    $('.break-time').text(breakClock.minutes + ":00");
 
 });
 // Remove minutes to break session
-$('.break-plus').click(function(){
-	breakClock.minutes++;
-	$(".break-time").text(breakClock.minutes + ":00");
+$('.break-plus').click(function() {
+    breakClock.minutes++;
+    $(".break-time").text(breakClock.minutes + ":00");
 
 });
 
 
 // if play is clicked, start sessionClock
-play.click(function(){
-	// pause.removeClass("hidden");
-	// play.addClass("hidden"); //TODO
-	$('.btn').addClass("hidden");
-	sessionClock.startTimer();
+play.click(function() {
+    // pause.removeClass("hidden");
+    // play.addClass("hidden"); //TODO
+    $('.btn').addClass("hidden");
+    sessionClock.startTimer();
+    b_time = setInterval(isBreakTime, 1000);
+
 });
 
 // TODO if pause is clicked, stop sessionClock
 
 
 // if reset button is clicked, reset sessionClock and breakClock
-reset.click(function(){
-	sessionClock.resetTimer();
-	// breakClock.resetTimer();
-	play.removeClass("hidden");
-	pause.addClass("hidden");
-	$('.btn').removeClass("hidden");
+reset.click(function() {
+    sessionClock.resetTimer();
+    // breakClock.resetTimer();
+    play.removeClass("hidden");
+    pause.addClass("hidden");
+    $('.btn').removeClass("hidden");
 });
 
 // if display timer is
-if(display.text() === "0:00"){
-	// start breakClock
-	$("body").css("background-color", "#008040");
-	$(".whiten").css("color", "#008040");
-	breakClock.startTimer();
+function seeBreak() {
+    $(".timer").addClass("hidden");
+    $(".break").removeClass("hidden");
+    $("#break-btn").addClass("whiten");
+    $('#session-btn').removeClass("whiten");
+}
+
+function seeSession() {
+    $(".break").addClass("hidden");
+    $(".timer").removeClass("hidden");
+    $('#session-btn').addClass("whiten");
+    $("#break-btn").removeClass("whiten");
+}
+
+function isBreakTime() {
+    if (display.text() === "0:00") {
+        clearInterval(b_time);
+        $(".timer").addClass("hidden");
+        $(".break").removeClass("hidden");
+        $("#break-btn").addClass("whiten");
+        $('#session-btn').removeClass("whiten");
+        $("body").css("background-color", "#008040");
+        $(".whiten").css("color", "#008040");
+        breakClock.startTimer();
+    }
 }
